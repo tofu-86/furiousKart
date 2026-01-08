a48c5a5b3f28f88b5152b24c1451b6a25b573d26
vanish <79671122+tofu-86@users.noreply.github.com>
2026-01-08 00:29:05 +0000

    Update CameraController.cs

--- furiousKart/Assets/Scripts/CameraController.cs
+++ furiousKart/Assets/Scripts/CameraController.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-
-
-
     public Transform player;
-
-
     private Rigidbody rb;
-
     public Vector3 Offset;
-
     public float speed;
 
     // Start is called before the first frame update
@@ -26,7 +19,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-
         Vector3 playerForward = (rb.velocity + player.transform.forward).normalized;
         transform.position = Vector3.Lerp(transform.position,
             player.position+player.TransformVector(Offset)+playerForward*(-5f),
